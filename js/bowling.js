102a1ef9ca869c7358a7f3d350aa69a0d322d7d5
rodrigovallades <rodrigo.vallades@gmail.com>
2018-02-17 20:11:38 -0200

    Instantiating bowling game

--- js/bowling.js
+++ js/bowling.js
@@ -177,3 +177,34 @@ class Bowling {
   }
 
 }
+
+(function() {
+  var bowlGameEl,
+      playBtn
+
+  const bowlingGame = new Bowling()
+
+  function renderGame() {
+    bowlGameEl.innerHTML = bowlingGame.render()
+  };
+
+  function onLoad() {
+    bowlingGame.init()
+    bowlGameEl = document.getElementById('bowling')
+    playBtn = document.getElementById('bowling-btn')
+    playBtn.addEventListener('click', play, false)
+  };
+
+  function play() {
+    bowlingGame.roll()
+    renderGame()
+
+    if (bowlingGame.isFinished()) onFinish()
+  };
+
+  function onFinish() {
+    playBtn.remove()
+  };
+
+  document.addEventListener('DOMContentLoaded', onLoad)
+}())
