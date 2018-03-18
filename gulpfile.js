cd0ce1fda01e6fdc51f3b3732f9c9a2fb25ccbac
rodrigovallades <rodrigo.vallades@gmail.com>
2018-03-18 01:40:32 -0300

    Initial dev build

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,8 @@ require('./gulp/tasks/watch')
 require('./gulp/tasks/scripts')
 require('./gulp/tasks/modernizr')
 require('./gulp/tasks/build')
+
+// init dev build
+var gulp = require('gulp')
+gulp.start('cssInject')
+gulp.start('scriptsRefresh')
